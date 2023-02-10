4725453866dbf96230b3ff9853d28db0469fa730
Brandon <bwin.cr7@gmail.com>
2023-02-09 20:25:56 -0800

    phase 3

--- authenticate-me/backend/db/models/user.js
+++ authenticate-me/backend/db/models/user.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcryptjs');
 const {
   Model,
   Validator
@@ -6,6 +7,40 @@ const {
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
+    toSafeObject() {
+      const { id, username, email } = this; // context will be the User instance
+      return { id, username, email };
+    };
+
+    static getCurrentUserById(id) {
+      return User.scope("currentUser").findByPk(id);
+    };
+
+    static async login({ credential, password }) {
+      const { Op } = require('sequelize');
+      const user = await User.scope('loginUser').findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential
+          }
+        }
+      });
+
+      if (user && user.validatePassword(password)) {
+        return await User.scope('currentUser').findByPk(user.id);
+      }
+    };
+
+    static async signup({ username, email, password }) {
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({
+        username,
+        email,
+        hashedPassword
+      });
+      return await User.scope('currentUser').findByPk(user.id);
+    };
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -45,11 +80,32 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       validate: {
         len: [60, 60]
+      },
+      validatePassword(password) {
+        return bcrypt.compareSync(password, this.hashedPassword.toString());
       }
     }
-  }, {
+  },
+  {
     sequelize,
-    modelName: 'User',
-  });
-  return User;
+    modelName: "User",
+    defaultScope: {
+      attributes: {
+        exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+      }
+    },
+    scopes: {
+      currentUser: {
+        attributes: { exclude: ["hashedPassword"] },
+
+      },
+      loginUser: {
+        attributes: {}
+      }
+    }
+  }
+);
+
+
+return User;
 };
