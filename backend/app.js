aa23f0962dbb1ec8b8c6c7353b218488588c16e3
Brandon <bwin.cr7@gmail.com>
2023-02-11 13:42:57 -0800

    help code

--- backend/app.js
+++ backend/app.js
@@ -5,64 +5,53 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-  // backend/app.js
-// ...
-const { ValidationError } = require('sequelize');
 
-// ...
-
-// backend/app.js
-const routes = require('./routes');
-// ...
-
-
-// backend/config/index.js
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
+const { ValidationError } = require('sequelize');
+
 const app = express();
+
+const routes = require('./routes');
+
 app.use(morgan('dev'));
 
 app.use(cookieParser());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send("LLLLLLLLLLLL")
+})
 
 // Security Middleware
 if (!isProduction) {
-  // enable cors only in development
-  app.use(cors());
-}
+    // enable cors only in development
+    app.use(cors());
+  }
 
-// helmet helps set a variety of headers to better secure your app
-app.use(
-  helmet.crossOriginResourcePolicy({
-    policy: "cross-origin"
-  })
-);
+  // helmet helps set a variety of headers to better secure your app
+  app.use(
+    helmet.crossOriginResourcePolicy({
+      policy: "cross-origin"
+    })
+  );
 
-// Set the _csrf token and create req.csrfToken method
-app.use(
-  csurf({
-    cookie: {
-      secure: isProduction,
-      sameSite: isProduction && "Lax",
-      httpOnly: true
-    }
-  })
-);
+  // Set the _csrf token and create req.csrfToken method
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
 
+  app.use(routes); // Connect all the routes
 
-app.use(routes); // Connect all the routes
-
-
-
-// backend/app.js "JG2uBp1n-yYnAzB6N_-zDAD_9YQ-01TzCcmI"
-// ...
-
-// backend/app.js
-// ...
-// Catch unhandled requests and forward to error handler.
-app.use((_req, _res, next) => {
+  // Catch unhandled requests and forward to error handler.
+  app.use((_req, _res, next) => {
     const err = new Error("The requested resource couldn't be found.");
     err.title = "Resource Not Found";
     err.errors = ["The requested resource couldn't be found."];
@@ -70,10 +59,8 @@ app.use((_req, _res, next) => {
     next(err);
   });
 
-
-
-// Process sequelize errors
-app.use((err, _req, _res, next) => {
+  // Process sequelize errors
+  app.use((err, _req, _res, next) => {
   // check if error is a Sequelize error:
   if (err instanceof ValidationError) {
     err.errors = err.errors.map((e) => e.message);
@@ -82,19 +69,132 @@ app.use((err, _req, _res, next) => {
   next(err);
 });
 
-
-// backend/app.js
-// ...
 // Error formatter
 app.use((err, _req, res, _next) => {
-    res.status(err.status || 500);
-    console.error(err);
-    res.json({
-      title: err.title || 'Server Error',
-      message: err.message,
-      errors: err.errors,
-      stack: isProduction ? null : err.stack
-    });
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
   });
+});
 
-module.exports = app;
+
+
+
+
+
+  module.exports = app;
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////
+// const express = require('express');
+// require('express-async-errors');
+// const morgan = require('morgan');
+// const cors = require('cors');
+// const csurf = require('csurf');
+// const helmet = require('helmet');
+// const cookieParser = require('cookie-parser');
+//   // backend/app.js
+// // ...
+// const { ValidationError } = require('sequelize');
+
+// // ...
+
+// // backend/app.js
+// const routes = require('./routes');
+// // ...
+
+
+// // backend/config/index.js
+// const { environment } = require('./config');
+// const isProduction = environment === 'production';
+
+// const app = express();
+// app.use(morgan('dev'));
+
+// app.use(cookieParser());
+// app.use(express.json());
+
+
+// // Security Middleware
+// if (!isProduction) {
+//   // enable cors only in development
+//   app.use(cors());
+// }
+
+// // helmet helps set a variety of headers to better secure your app
+// app.use(
+//   helmet.crossOriginResourcePolicy({
+//     policy: "cross-origin"
+//   })
+// );
+
+// // Set the _csrf token and create req.csrfToken method
+// app.use(
+//   csurf({
+//     cookie: {
+//       secure: isProduction,
+//       sameSite: isProduction && "Lax",
+//       httpOnly: true
+//     }
+//   })
+// );
+
+// console.log("LLLLLLLLLLLLLLLLLLLLLLLLLLL")
+// app.use(routes); // Connect all the routes
+
+
+
+// // backend/app.js "JG2uBp1n-yYnAzB6N_-zDAD_9YQ-01TzCcmI"
+// // ...
+
+// // backend/app.js
+// // ...
+// // Catch unhandled requests and forward to error handler.
+// app.use((_req, _res, next) => {
+//     const err = new Error("The requested resource couldn't be found.");
+//     err.title = "Resource Not Found";
+//     err.errors = ["The requested resource couldn't be found."];
+//     err.status = 404;
+//     next(err);
+//   });
+
+
+
+// // Process sequelize errors
+// app.use((err, _req, _res, next) => {
+//   // check if error is a Sequelize error:
+//   if (err instanceof ValidationError) {
+//     err.errors = err.errors.map((e) => e.message);
+//     err.title = 'Validation error';
+//   }
+//   next(err);
+// });
+
+
+// // backend/app.js
+// // ...
+// // Error formatter
+// app.use((err, _req, res, _next) => {
+//     res.status(err.status || 500);
+//     console.error(err);
+//     res.json({
+//       title: err.title || 'Server Error',
+//       message: err.message,
+//       errors: err.errors,
+//       stack: isProduction ? null : err.stack
+//     });
+//   });
+
+// module.exports = app;
