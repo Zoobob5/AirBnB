ce7940b0a6468e49b31c7d32dde83cd62fe3bf03
Brandon <bwin.cr7@gmail.com>
2023-02-15 21:27:10 -0800

    fix some api/session bits

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -65,7 +65,7 @@ router.get(
         return res.json({
           user: user.toSafeObject()
         });
-      } else return res.json({});
+      } else return res.json({user: null});
     }
   );
 
