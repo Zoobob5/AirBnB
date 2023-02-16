fdca51d6674d2c255c56dc373793d0ec9f73b9aa
Brandon <bwin.cr7@gmail.com>
2023-02-16 04:56:50 -0800

    add migrations and models for spots

---
+++ backend/db/migrations/20230216105703-create-spot.js
@@ -0,0 +1,77 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+const { sequelize } = require("../models");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Spots', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ownerId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id"
+        }
+      },
+      address: {
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      city: {
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      state: {
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      country: {
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      lat: {
+        allowNull:false,
+        type: Sequelize.FLOAT
+      },
+      lng: {
+        allowNull:false,
+        type: Sequelize.FLOAT
+      },
+      name: {
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      price: {
+        allowNull:false,
+        type: Sequelize.DECIMAL(10,2)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Spots');
+  }
+};
