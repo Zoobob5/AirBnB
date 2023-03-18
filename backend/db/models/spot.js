6a4bee21abd943c77d657cfb8ef84cd5edbdc475
Brandon <bwin.cr7@gmail.com>
2023-03-18 14:44:43 -0700

    fix migrations part3

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -56,7 +56,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
     },
     price:{
-      type: DataTypes.DECIMAL(10, 2),
+      type: DataTypes.DECIMAL,
       allowNull: false,
     },
   }, {
